e5ca51decbbe0f07053d1dce5d3689bd3c77fc72
osvaldobnu <osvaldobnu@gmail.com>
2026-06-27 17:35:38 -0300

    Ajustes...

--- sw.js
+++ sw.js
@@ -1,14 +1,40 @@
-const CACHE_NAME = "explorer-v1"
+const CACHE_NAME = "explorer-v1";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js"
+];
 
+// Instalação do Service Worker
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll([
-                "/",
-                "index.html",
-                "style.css",
-                "script.js"
-            ])
+            return cache.addAll(ASSETS);
         })
     );
-})
+});
+
+// Ativação e limpeza de caches antigos
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Estratégia de Cache: Serve o cache primeiro, depois busca na rede
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
